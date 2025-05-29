8ea2a500e2d4d0819e63833643ff5e5fcf64233c
MarioPaerle <paerle.music@gmail.com>
2025-05-29 22:48:58 +0200

    New Typologied in the StChopiner

--- StChopiner.py
+++ StChopiner.py
@@ -190,11 +190,16 @@ from io import BytesIO
 def midi_to_bytes(midi_obj):
     midi_bytes_io = BytesIO()
     midi_obj.write(midi_bytes_io)
-    return midi_bytes_io.getvalue()
+    midi_bytes_io.seek(0)  # Reset pointer to start of buffer
+    return midi_bytes_io
 
 if st.button("generate"):
     waltzer, midi = chopiner()
 
     st.pyplot(waltzer.plot())
     st.audio(waltzer.toaudio(), sample_rate=44_100)
-    st.download_button("Download Midi file", data=midi_to_bytes(midi))
+    st.download_button("Download Midi file",
+                       data=midi_to_bytes(midi),
+                       file_name="generated_music.mid",
+                       mime="audio/midi"
+    )
